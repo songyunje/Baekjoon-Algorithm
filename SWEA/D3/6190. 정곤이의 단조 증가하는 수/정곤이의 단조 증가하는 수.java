24980dc16eee71815840dc69025229417aecc72d
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-02-13 14:48:36 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 411 ms, Memory: 31,340 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -12,9 +12,8 @@ public class Solution {
 		
 
 		for(int t=0;t<T;t++) {
-			int max = 0;
+			int max = -1;
 			int mlt;
-			int ans;
 			StringBuilder sb = new StringBuilder("#");
 			int N = Integer.parseInt(br.readLine());
 			
@@ -23,44 +22,20 @@ public class Solution {
 			for(int i=0;i<N;i++) {
 				arr[i]=Integer.parseInt(st.nextToken());
 			}
-			if(arr.length==1) {
+			if(N==1) {
 				max=arr[0];
 			}
-			
 			for(int i=0;i<N-1;i++) {
 				for(int j=i+1;j<N;j++) {
 					mlt = arr[i]*arr[j];
-					String mltS = Integer.toString(arr[i]*arr[j]);
-					int status=0;
-					if(mltS.length()==1) {
-						status=1;
-					}
-					else {
-						for(int k=0;k<mltS.length()-1;k++) {
-							if((mltS.charAt(k)-'0')<=(mltS.charAt(k+1)-'0')) {
-								status=1;
-							}
-							else {
-								status=0;
-								break;
-							}
-						}
-					}
-					
-					if(status==1) {
+					if(check(mlt)) {
 						max=Math.max(max, mlt);
 					}
-						
 				}
 			}
-			if(max==0) {
-				ans=-1;
-			}
-			else {
-				ans=max;
-			}
-			
-			sb.append((t+1)).append(" ").append(ans).append("\n");
+	
+		
+			sb.append((t+1)).append(" ").append(max).append("\n");
 			
 			bw.write(String.valueOf(sb));
 			
@@ -71,4 +46,19 @@ public class Solution {
 		bw.close();
 
 	} 
+	
+	public static boolean check(int num) {
+		int a=9;
+		int b;
+		while(num>0) {
+			
+			b=num%10;
+			if(b>a) {
+				return false;
+			}
+			num=num/10;
+			a=b;
+		}
+		return true;
+	}
 }
