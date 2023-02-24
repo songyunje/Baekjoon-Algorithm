917be4ba1e14106cc909b645e76a699bcdf5ac06
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-02-24 10:27:08 +0900

    [Gold II] Title: K번째 수, Time: 172 ms, Memory: 15568 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main  {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int K = Integer.parseInt(br.readLine());
+		
+
+		int start = 1;
+		int end = K;
+		
+		while(start<end) {
+			int count = 0;
+			int mid = (start+end)/2;
+			for(int i=1;i<=N;i++) {
+				count+=Math.min(mid/i, N);
+			}
+			if(count>=K) {
+				end=mid;
+
+			}
+			else {
+				start=mid+1;
+			}
+		}
+
+		
+		bw.write(String.valueOf(end));
+
+		
+		br.close();
+		bw.flush();
+		bw.close();
+		
+	}
+}
