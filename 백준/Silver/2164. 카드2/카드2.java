c26e2ad030ce2b153040518debea067570df3990
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-02-28 23:54:20 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 49060 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer> q = new LinkedList<>();
+		int N = sc.nextInt();
+		for(int i=1;i<=N;i++) {
+			q.add(i);
+		}
+		while(q.size()>1) {
+			q.poll();
+			q.add(q.poll());
+		}
+		System.out.println(q.poll());
+	}
+}
