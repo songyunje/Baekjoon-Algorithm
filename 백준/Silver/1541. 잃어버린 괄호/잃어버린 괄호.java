a12a8a5afdca7dad5453fc145dbb7574a6c8e637
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-03-13 15:23:19 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 132 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine().trim(),"-");
+		
+		Boolean check = false;
+		int ans = 0;
+		while(st.hasMoreTokens()) {
+			int temp = 0;
+			StringTokenizer str = new StringTokenizer(st.nextToken(),"+");
+			while(str.hasMoreTokens()) {
+				temp+=Integer.parseInt(str.nextToken());
+			}
+			if(!check)
+				ans+=temp;
+			else
+				ans-=temp;
+			check=true;
+		}
+		
+		bw.write(String.valueOf(ans));
+		br.close();
+		bw.close();
+	}
+
+}
