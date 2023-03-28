9ffc6a7220c654a4b845c607d4367ab9ba7929e2
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-03-28 23:57:32 +0900

    [Silver III] Title: 1로 만들기, Time: 220 ms, Memory: 21660 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] dp = new int[1000001];
+		dp[1]=0;
+		dp[2]=1;
+		dp[3]=1;
+		for(int i=4;i<=N;i++) {
+			dp[i]=dp[i-1]+1;
+			if(i%2==0) dp[i]=Math.min(dp[i], dp[i/2]+1);
+			if(i%3==0) dp[i]=Math.min(dp[i], dp[i/3]+1);
+		} // dp
+		System.out.println(dp[N]);
+	} // main
+}
