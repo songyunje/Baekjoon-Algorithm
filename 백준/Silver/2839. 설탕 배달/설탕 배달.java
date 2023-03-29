4fe31339cada4b6ef4e5e2f97eb949013110ce01
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-03-29 10:02:49 +0900

    [Silver IV] Title: 설탕 배달, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int temp = N/5;
+		
+		switch(N%5) {
+		case 0 : System.out.println(temp); break;
+		case 1 : if(temp>=1) System.out.println(temp+1);
+				else System.out.println(-1); break;
+		case 2 : if(temp>=2) System.out.println(temp+2);
+				else System.out.println(-1); break;
+		case 3 : System.out.println(temp+1); break;
+		case 4 : if(temp>=1) System.out.println(temp+2);
+		        else System.out.println(-1); break;
+		}
+	}
+
+}
