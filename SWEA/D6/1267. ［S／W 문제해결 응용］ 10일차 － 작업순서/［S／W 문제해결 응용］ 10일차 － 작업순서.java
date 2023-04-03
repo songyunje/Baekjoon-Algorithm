310cf17ca433af86da05e363140ed3f1eaa76e73
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-04-03 12:05:40 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 134 ms, Memory: 24,140 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -1,103 +1,52 @@
 import java.io.*;
 import java.util.*;
-
 public class Solution {
+	static int V,E;
+	static List<Integer>[] arr;
+	static int[] level;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		StringBuilder sb;
+		StringBuilder sb = new StringBuilder();
 		
-		for(int t=0;t<10;t++) {
-			sb = new StringBuilder("#");
-			st = new StringTokenizer(br.readLine()," ");
-			
-			int V = Integer.parseInt(st.nextToken());
-			int E = Integer.parseInt(st.nextToken());
-			
-			int[] arr = new int[V+1];
-			
-			for(int i=1;i<V+1;i++) {
-				arr[i]=i;
+		for(int t=1;t<=10;t++) {
+			st = new StringTokenizer(br.readLine());
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			arr = new ArrayList[V+1];
+			for(int i=1;i<=V;i++) {
+				arr[i]=new ArrayList<>();
 			}
+			level = new int[V+1];
 			
-			st = new StringTokenizer(br.readLine()," ");
-			
-			int[][] map = new int[V+1][V+1];
-			
-			for(int e=0;e<E;e++) {
-				int i = index(arr, Integer.parseInt(st.nextToken()));
-				int j = index(arr, Integer.parseInt(st.nextToken()));
-				map[i][j]++;
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<E;i++) {
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[a].add(b);
+				level[b]++;
 			}
-			
-//			for(int i=0;i<V+1;i++) {
-//				for(int j=0;j<V+1;j++) {
-//					System.out.print(map[i][j]);
-//				}
-//				System.out.println();
-//			}
-			
-			for(int a=0;a<V;a++) {
-				for(int i=V;i>=0;i--) {
-					int min = V;
-					int none =0;
-					for(int j=0;j<V+1;j++) {
-						if(map[i][j]==1) {
-							min = Math.min(min, index(arr,j)); 
-						}	
-					}
-//					System.out.println(min);
-					if(index(arr,i)>min)
-						shift(arr,min,index(arr,i));
-//					System.out.println(Arrays.toString(arr));
-				}
-			}
-			
-			
-			sb.append((t+1)+" ");
-			
-			for(int i=1;i<V+1;i++) {
-				sb.append(arr[i]+" ");
-			}
-			
-			sb.append("\n");
-			
-			bw.write(sb.toString());
+			sb.append("#"+t+" "+top()+"\n");	
 		}
-		
+		bw.write(sb.toString());
 		br.close();
-		bw.flush();
-		bw.close();
-	}
-//	public static void main(String[] args) {
-//		int[] array = {1,3,5,6,9,10};
-//		
-//		int ans = index(array,0,5,9);
-//		System.out.println(ans);
-//	}
-	
-	
-	
-	
-	static int index(int[] arr, int key) {
-		int index = 0;
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i]==key) 
-				index=i;
+		bw.close();		
+	} // main
+	static String top() {
+		StringBuilder tsb = new StringBuilder();
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=1;i<=V;i++) {
+			if(level[i]==0) q.add(i);
 		}
-		return index;
-	}
-	
-	
-	
-	static void shift(int[] arr, int a, int b) {
-		int temp = arr[b];
-		while(b>a) {
-			arr[b]=arr[b-1];
-			b--;
+		while(!q.isEmpty()) {
+			int temp = q.poll();
+			tsb.append(temp+" ");
+			for(int i : arr[temp]) {
+				level[i]--;
+				if(level[i]==0) q.add(i);
+			}
 		}
-		arr[a]=temp;
+		return tsb.toString();
 	}
-
 }
