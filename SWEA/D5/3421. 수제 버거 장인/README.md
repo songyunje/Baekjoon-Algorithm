c56c287869fce898173473792bf777dde1f98d8a
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-04-07 12:33:50 +0900

    [D5] Title: 수제 버거 장인, Time: 107 ms, Memory: 18,760 KB -BaekjoonHub

---
+++ SWEA/D5/3421. 수제 버거 장인/README.md
@@ -0,0 +1,11 @@
+# [D5] 수제 버거 장인 - 3421 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWErcQmKy6kDFAXi) 
+
+### 성능 요약
+
+메모리: 18,760 KB, 시간: 107 ms, 코드길이: 1,455 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/3421. 수제 버거 장인/수제 버거 장인.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+	static int N,cnt;
+	static int[] ig;
+	static int[] check;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;	
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int t=1;t<=T;t++) {
+        	st = new StringTokenizer(br.readLine());
+        	N = Integer.parseInt(st.nextToken());
+        	int M = Integer.parseInt(st.nextToken());
+        	cnt=0;
+        	ig = new int[N+1];
+        	check = new int[N+1];
+        	for(int i=1;i<=N;i++) {
+        		ig[i]=i;
+        	}
+        	for(int m=0;m<M;m++) {
+        		st = new StringTokenizer(br.readLine());
+            	int v = Integer.parseInt(st.nextToken());
+            	int e = Integer.parseInt(st.nextToken());
+            	
+            	check[v]=check[v]|(1<<e);
+            	check[e]=check[e]|(1<<v);
+        	}
+        	burger(1,0);
+        	
+        	sb.append("#"+t+" "+cnt+"\n");
+        } // testcase
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+	} // main
+	static void burger(int a, int b) {
+		if(a>N) {
+			cnt++;
+			return;
+		}
+		if((b&check[a])>0) burger(a+1,b);
+		else {
+			burger(a+1,b);
+			burger(a+1,b|(1<<a));
+		}
+	} // burger
+
+}
