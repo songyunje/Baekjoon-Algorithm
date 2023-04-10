013ddc6c58c2f5845c9116e7952ac8aa469971c4
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-04-10 11:45:29 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
  
 public class Main {
  
@@ -9,28 +8,20 @@ public class Main {
  
 		int N = Integer.parseInt(br.readLine());
  
-		int[] DP = new int[N + 1];
-		int[] arr = new int[N + 1];
+		int[] dp = new int[N + 1];
  
- 
+		int pre=0;
+		int temp=0;
 		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			pre=temp;
+			temp = Integer.parseInt(br.readLine());
+			if(i==1) dp[1]=temp;
+			else if(i==2) dp[2]= dp[1]+temp;
+			else dp[i]=Math.max(dp[i-2], dp[i-3]+pre)+temp;
 		}
- 
-		// index = 0 은 시작점이다.
-		DP[1] = arr[1];
 		
-		// N 이 1이 입력될 수도 있기 때문에 예외처리를 해줄 필요가 있다.
-		if (N >= 2) {
-			DP[2] = arr[1] + arr[2];
-		}
- 
-		for (int i = 3; i <= N; i++) {
-			DP[i] = Math.max(DP[i - 2] , DP[i - 3] + arr[i - 1]) + arr[i];
-		}
- 
-		System.out.println(DP[N]);
- 
+		System.out.println(dp[N]);
+
 	}
  
 }
