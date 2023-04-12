cd34fefe28971fe8cfaf984a394566465d7efafe
SuperYunje <122420080+songyunje@users.noreply.github.com>
2023-04-12 22:41:46 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 58.44 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 116 MB, 시간: 48.51 ms
+메모리: 120 MB, 시간: 58.44 ms
 
 ### 구분
 
--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -13,14 +13,14 @@ class Solution {
             q1.add(queue1[i]);
             q2.add(queue2[i]);
         }
-        double targetSum = (sum1+sum2)/ 2.0;
+        double avg = (sum1+sum2)/ 2;
         while (!q1.isEmpty() && !q2.isEmpty()) {
-            if (sum1 < targetSum) {
+            if (sum1 < avg) {
                 int tmp = q2.poll();
                 sum1 += tmp;
                 q1.add(tmp);
                 answer += 1;
-            } else if (sum1 > targetSum) {
+            } else if (sum1 > avg) {
                 sum1 -= q1.poll();
                 answer += 1;
             } else {
